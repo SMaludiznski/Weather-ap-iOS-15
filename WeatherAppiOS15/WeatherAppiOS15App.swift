1623d39b7d0a42635033290600777c1aab92eb19
Sebastian <sebastianmaludzinski@icloud.com>
2021-10-11 20:42:01 -0700

    Initial Commit

---
+++ WeatherAppiOS15/WeatherAppiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppiOS15App.swift
+//  WeatherAppiOS15
+//
+//  Created by Sebastian Maludziński on 11/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
